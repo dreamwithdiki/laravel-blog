04906d0f86ea0d2b0e2531906baf69586bcf77b7
dev-diki <fachrealheart@engineer.com>
2022-02-17 18:10:53 +0700

    authenticate login | middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,10 +21,31 @@
          
         </ul>
         <ul class="navbar-nav ms-auto">
+          @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Welcome back, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-house"></i> My Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                  <form action="/logout" method="post">
+                    @csrf
+                    <button class="dropdown-item" type="submit">
+                      <i class="bi bi-box-arrow-in-right"></i> Logout
+                    </button>
+                  </form>
+              </li>
+            </ul>
+          </li>
+          @else
           <li class="nav-item">
               <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-right"></i> Login</a>
           </li>
+          @endauth
         </ul>
+
       </div>
     </div>
   </nav>
